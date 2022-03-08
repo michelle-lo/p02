747c4dc92a7e55c72cf49ca357d392aafa4cc2ee
michelle.lo <mlo20@stuy.edu>
2022-03-08 09:02:15 -0500

    bare bones flask app -- m more to come

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,18 @@
 # SoftDev
 # P02: Four Toppings Boba Shop
 # 2022-03-06
+
+
+from flask import Flask
+from flask import request, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def welcome():
+    return "Boba"
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
