557a485d6f5371e7d96df6be7cfb826a5cca7103
AnnabelZhang1 <annabel.zhang1@gmail.com>
2022-03-17 09:18:29 -0400

    added third layer

--- app/static/js/counter.js
+++ app/static/js/counter.js
@@ -8,12 +8,15 @@
 //Different canvas layers
 var c1 = document.getElementById('counter');
 var c2 = document.getElementById('kitchen');
+var c3 = document.getElementById('drink');
 var counterB = document.getElementById('countStage');
 var kitchenB = document.getElementById('kitchenStage');
 
 // '''prepare to interact with canvas in 2D'''
 var ctx1 = c1.getContext("2d");
 var ctx2 = c2.getContext("2d");
+var ctx3 = c3.getContext("2d");
+
 
 
 var drawCounter = () => {
@@ -28,18 +31,26 @@ var drawKitchen = () => {
 };
 
 // // creates background images for counter on canvas
+let img0 = document.createElement("img");
+img0.src = '../static/assets/cup.png';
+
+img0.addEventListener("load", () => {
+  ctx3.drawImage(img0, 0, 0)
+});
+
+
 let img1 = document.createElement("img");
-img1.src = '../static/img/flower1.png';
+img1.src = '../static/img/counter_canvas.png';
 
 img1.addEventListener("load", () => {
-  ctx1.drawImage(img1, c1.clientWidth/2, c1.clientHeight/2)
+  ctx1.drawImage(img1, 0, 0)
 });
 
 let img2 = document.createElement("img");
-img2.src = '../static/img/flower2.png';
+img2.src = '../static/img/kitchen_canvas.png';
 
 img2.addEventListener("load", () => {
-  ctx2.drawImage(img2, c2.clientWidth/2, c2.clientHeight/2)
+  ctx2.drawImage(img2, 0, 0)
 });
 
 counterB.addEventListener("click", drawCounter);
