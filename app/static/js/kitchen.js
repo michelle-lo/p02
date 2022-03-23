f12345cde9fe9ea0a40d46bbe537b7a4e56e4226
AnnabelZhang1 <annabel.zhang1@gmail.com>
2022-03-23 19:59:26 -0400

    drink validation complete

--- app/static/js/kitchen.js
+++ app/static/js/kitchen.js
@@ -7,9 +7,10 @@
 // '''access canvas and buttons via DOM'''
 //Different canvas layers
 
-// var c1 = document.getElementById('counter');
-var c2 = document.getElementById('kitchen');
-var c3 = document.getElementById('drink');
+var c1 = document.getElementById('kitchen');
+var c2 = document.getElementById('cup');
+var c3 = document.getElementById('tea');
+var c4 = document.getElementById('topping');
 
 // drink variable
 var drink = {tea:null, topp1:null, topp2:null};
@@ -18,8 +19,10 @@ var toppSet = 0;
 
 // '''prepare to interact with canvas in 2D'''
 // var ctx1 = c1.getContext("2d");
+var ctx1 = c1.getContext("2d");
 var ctx2 = c2.getContext("2d");
 var ctx3 = c3.getContext("2d");
+var ctx4 = c4.getContext("2d");
 
 
 // // creates background images for counter on canvas
@@ -27,14 +30,14 @@ let img0 = document.createElement("img");
 img0.src = '../static/assets/cup.png';
 
 img0.addEventListener("load", () => {
-  ctx3.drawImage(img0, 0, 0)
+  ctx2.drawImage(img0, 0, 0)
 });
 
 let img2 = document.createElement("img");
 img2.src = '../static/img/kitchen_canvas.png';
 
 img2.addEventListener("load", () => {
-  ctx2.drawImage(img2, 0, 0)
+  ctx1.drawImage(img2, 0, 0)
 });
 
 function addTea(teatype){
@@ -85,13 +88,14 @@ function addTopp(topping){
     drink["topp1"] = topping;}
   else{
     drink["topp2"] = topping}
-}
+  }
+
 
 let img7 = document.createElement("img");
 img7.src = '../static/assets/topping_lychee.png'
 
 function drawLycheeJelly(){
-    ctx3.drawImage(img7, 0, 0);
+    ctx4.drawImage(img7, 0, 0);
     addTopp("lychee");
 }
 
@@ -99,7 +103,7 @@ let img8 = document.createElement("img");
 img8.src = '../static/assets/topping_tapioca.png'
 
 function drawTapioca(){
-    ctx3.drawImage(img8, 0, 0);
+    ctx4.drawImage(img8, 0, 0);
     addTopp("tapioca");
 }
 
@@ -107,7 +111,7 @@ let img9 = document.createElement("img");
 img9.src = '../static/assets/topping_redbean.png'
 
 function drawRedBean(){
-    ctx3.drawImage(img9, 0, 0);
+    ctx4.drawImage(img9, 0, 0);
     addTopp("redBean");
 }
 
@@ -115,7 +119,7 @@ let img10 = document.createElement("img");
 img10.src = '../static/assets/topping_milkfoam.png'
 
 function drawMilkFoam(){
-    ctx3.drawImage(img10, 0, 0);
+    ctx4.drawImage(img10, 0, 0);
     addTopp("milkFoam");
 }
 
@@ -123,13 +127,14 @@ let img11 = document.createElement("img");
 img11.src = '../static/assets/topping_grassjelly.png'
 
 function drawGrassJelly(){
-    ctx3.drawImage(img11, 0, 0);
+    ctx4.drawImage(img11, 0, 0);
     addTopp("grassJelly");
 }
 
 function clearDrink(){
     ctx3.clearRect(0, 0, c3.width, c3.height);
-    ctx3.drawImage(img0, 0, 0)
+    ctx4.clearRect(0, 0, c3.width, c3.height);
+    // ctx3.drawImage(img0, 0, 0)
     drink = {tea:null, topp1:null, topp2:null};
     teaSet = false;
     toppSet = 0;
@@ -168,7 +173,9 @@ var draw = (e) => {
   var mouseY = e.offsetY
   console.log("mouseclick registered at ", mouseX, mouseY);
 
-  // teass
+  // TEAS
+
+  // if tea is not set yet
   if (teaSet === false) {
     if (mouseY <= 390 && mouseY >= 350 && mouseX <= 120 && mouseX >= 40){
       drawMilkTea();
@@ -186,8 +193,17 @@ var draw = (e) => {
       drawOolongTea();
       teaSet = true;
     }
+    // stops user trying to choose a topping before a tea (ex: milk foam floating in midair)
+    else if ((mouseY <= 285 && mouseY >= 200 && mouseX <= 280 && mouseX >= 210) ||
+             (mouseY <= 270 && mouseY >= 230 && mouseX <= 440 && mouseX >= 340) ||
+             (mouseY <= 330 && mouseY >= 290 && mouseX <= 415 && mouseX >= 315) ||
+             (mouseY <= 390 && mouseY >= 355 && mouseX <= 390 && mouseX >= 290) ||
+             (mouseY <= 270 && mouseY >= 230 && mouseX <= 585 && mouseX >= 485))
+      alert("You need to add a tea before adding any toppings!");
 
   }
+
+  // if tea is set
   else if (teaSet === true) {
     // stops alert from showing if you click outside hitbox
     if ((mouseY <= 390 && mouseY >= 350 && mouseX <= 120 && mouseX >= 40) ||
@@ -195,48 +211,81 @@ var draw = (e) => {
         (mouseY >= 300 && mouseY <= 340 && mouseX <= 130 && mouseX >= 50) ||
         (mouseY >= 300 && mouseY <= 340 && mouseX <= 225 && mouseX >= 150))
     alert("You have already chosen a tea type! \nRestart your drink to choose a different tea.")
-  }
+
+    // if only zero or one topping is set
+    if (toppSet === 0 || toppSet === 1){
+      if (mouseY <= 285 && mouseY >= 200 && mouseX <= 280 && mouseX >= 210){
+        drawMilkFoam();
+        toppSet += 1;
+      }
+      else if (mouseY <= 270 && mouseY >= 230 && mouseX <= 440 && mouseX >= 340){
+        drawLycheeJelly();
+        toppSet += 1;
+      }
+      else if (mouseY <= 330 && mouseY >= 290 && mouseX <= 415 && mouseX >= 315){
+        drawGrassJelly();
+        toppSet += 1;
+      }
+      else if (mouseY <= 390 && mouseY >= 355 && mouseX <= 390 && mouseX >= 290){
+        drawTapioca();
+        toppSet += 1;
+      }
+      else if (mouseY <= 270 && mouseY >= 230 && mouseX <= 585 && mouseX >= 485){
+        drawRedBean();
+        toppSet += 1;
+      }
+    }
+    // stops alert from showing if you click outside hitbox
+    else if (toppSet === 2){
+      if ((mouseY <= 285 && mouseY >= 200 && mouseX <= 280 && mouseX >= 210) ||
+          (mouseY <= 270 && mouseY >= 230 && mouseX <= 440 && mouseX >= 340) ||
+          (mouseY <= 330 && mouseY >= 290 && mouseX <= 415 && mouseX >= 315) ||
+          (mouseY <= 390 && mouseY >= 355 && mouseX <= 390 && mouseX >= 290) ||
+          (mouseY <= 270 && mouseY >= 230 && mouseX <= 585 && mouseX >= 485))
+        alert("You have already chosen two toppings! \nRestart your drink to choose a different combination of toppings.")
+    }
+  // }
   console.log(Object.values(drink));
 
 
   // toppingss
-  if (toppSet === 0 || toppSet === 1){
-    if (mouseY <= 285 && mouseY >= 200 && mouseX <= 280 && mouseX >= 210){
-      drawMilkFoam();
-      toppSet += 1;
-    }
-    else if (mouseY <= 270 && mouseY >= 230 && mouseX <= 440 && mouseX >= 340){
-      drawLycheeJelly();
-      toppSet += 1;
-    }
-    else if (mouseY <= 330 && mouseY >= 290 && mouseX <= 415 && mouseX >= 315){
-      drawGrassJelly();
-      toppSet += 1;
-    }
-    else if (mouseY <= 390 && mouseY >= 355 && mouseX <= 390 && mouseX >= 290){
-      drawTapioca();
-      toppSet += 1;
-    }
-    else if (mouseY <= 270 && mouseY >= 230 && mouseX <= 585 && mouseX >= 485){
-      drawRedBean();
-      toppSet += 1;
-    }
-  }
-  else if (toppSet === 2){
-    if ((mouseY <= 285 && mouseY >= 200 && mouseX <= 280 && mouseX >= 210) ||
-        (mouseY <= 270 && mouseY >= 230 && mouseX <= 440 && mouseX >= 340) ||
-        (mouseY <= 330 && mouseY >= 290 && mouseX <= 415 && mouseX >= 315) ||
-        (mouseY <= 390 && mouseY >= 355 && mouseX <= 390 && mouseX >= 290) ||
-        (mouseY <= 270 && mouseY >= 230 && mouseX <= 585 && mouseX >= 485))
-      alert("You have already chosen two toppings! \nRestart your drink to choose a different combination of toppings.")
-  }
+  // if (toppSet === 0 || toppSet === 1){
+  //   if (mouseY <= 285 && mouseY >= 200 && mouseX <= 280 && mouseX >= 210){
+  //     drawMilkFoam();
+  //     toppSet += 1;
+  //   }
+  //   else if (mouseY <= 270 && mouseY >= 230 && mouseX <= 440 && mouseX >= 340){
+  //     drawLycheeJelly();
+  //     toppSet += 1;
+  //   }
+  //   else if (mouseY <= 330 && mouseY >= 290 && mouseX <= 415 && mouseX >= 315){
+  //     drawGrassJelly();
+  //     toppSet += 1;
+  //   }
+  //   else if (mouseY <= 390 && mouseY >= 355 && mouseX <= 390 && mouseX >= 290){
+  //     drawTapioca();
+  //     toppSet += 1;
+  //   }
+  //   else if (mouseY <= 270 && mouseY >= 230 && mouseX <= 585 && mouseX >= 485){
+  //     drawRedBean();
+  //     toppSet += 1;
+  //   }
+  // }
+  // else if (toppSet === 2){
+  //   if ((mouseY <= 285 && mouseY >= 200 && mouseX <= 280 && mouseX >= 210) ||
+  //       (mouseY <= 270 && mouseY >= 230 && mouseX <= 440 && mouseX >= 340) ||
+  //       (mouseY <= 330 && mouseY >= 290 && mouseX <= 415 && mouseX >= 315) ||
+  //       (mouseY <= 390 && mouseY >= 355 && mouseX <= 390 && mouseX >= 290) ||
+  //       (mouseY <= 270 && mouseY >= 230 && mouseX <= 585 && mouseX >= 485))
+  //     alert("You have already chosen two toppings! \nRestart your drink to choose a different combination of toppings.")
+  // }
 
 
-}
+}}
 
 
 
-c3.addEventListener("click", draw)
+c4.addEventListener("click", draw);
 // counterB.addEventListener("click", drawCounter);
 // kitchenB.addEventListener("click", drawKitchen);
 // milkTea.addEventListener("click", drawMilkTea);
